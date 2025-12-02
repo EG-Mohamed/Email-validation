9f9fe781d8eaff0d76ebcc9643f0a522ae09a97c
Mohamed Said <personal@msaied.com>
2025-12-02 22:23:10 +0200

    WIP

--- src/Rules/EmailValidationRule.php
+++ src/Rules/EmailValidationRule.php
@@ -14,11 +14,20 @@ class EmailValidationRule implements ValidationRule
     {
         $this->service = new EmailValidatorService;
     }
+    protected function getAttributeName(string $attribute): string
+    {
+        $fieldName = last(explode('.', $attribute));
 
+        return trans_choice('validation.attributes.' . $fieldName, 1)
+            ?: trans_choice('validation.attributes.' . $attribute, 1)
+                ?: str_replace(['_', '.'], ' ', $fieldName);
+    }
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
+
+        $attributeName = $this->getAttributeName($attribute);
         if (! is_string($value)) {
-            $fail(__('email-validation::email-validation.syntax', ['attribute' => $attribute]));
+            $fail(__('email-validation::email-validation.syntax', ['attribute' => $attributeName]));
             return;
         }
 
@@ -26,21 +35,21 @@ class EmailValidationRule implements ValidationRule
 
         if ($config['syntax'] ?? true) {
             if (! $this->service->validateSyntax($value)) {
-                $fail(__('email-validation::email-validation.syntax', ['attribute' => $attribute]));
+                $fail(__('email-validation::email-validation.syntax', ['attribute' => $attributeName]));
                 return;
             }
         }
 
         if ($config['disposable'] ?? true) {
             if (! $this->service->validateDisposable($value)) {
-                $fail(__('email-validation::email-validation.disposable', ['attribute' => $attribute]));
+                $fail(__('email-validation::email-validation.disposable', ['attribute' => $attributeName]));
                 return;
             }
         }
 
         if ($config['dns'] ?? true) {
             if (! $this->service->validateDns($value)) {
-                $fail(__('email-validation::email-validation.dns', ['attribute' => $attribute]));
+                $fail(__('email-validation::email-validation.dns', ['attribute' => $attributeName]));
                 return;
             }
         }
