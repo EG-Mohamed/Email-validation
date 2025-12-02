ad068148117fa89b8e8ea44b4b98afcaa299e28a
EG-Mohamed <23424932+EG-Mohamed@users.noreply.github.com>
2025-12-02 20:29:10 +0000

    Fix styling

--- src/Rules/EmailValidationRule.php
+++ src/Rules/EmailValidationRule.php
@@ -18,20 +18,22 @@ class EmailValidationRule implements ValidationRule
     {
         if (! is_string($value)) {
             $fail('email-validation::email-validation.syntax')->translate();
+
             return;
         }
 
         $config = config('email-validation.validations', []);
 
         $validations = [
-            'syntax' => fn() => $this->service->validateSyntax($value),
-            'disposable' => fn() => $this->service->validateDisposable($value),
-            'dns' => fn() => $this->service->validateDns($value),
+            'syntax' => fn () => $this->service->validateSyntax($value),
+            'disposable' => fn () => $this->service->validateDisposable($value),
+            'dns' => fn () => $this->service->validateDns($value),
         ];
 
         foreach ($validations as $key => $validation) {
             if (($config[$key] ?? true) && ! $validation()) {
                 $fail("email-validation::email-validation.{$key}")->translate();
+
                 return;
             }
         }
